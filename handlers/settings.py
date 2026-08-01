80e61536860d27ecd8283259c1bc4fbb350bc2e3
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 01:03:17 +0330

    Update settings.py

--- handlers/settings.py
+++ handlers/settings.py
@@ -1,1 +1,34 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from database.db import connect_db
+
+
+
+async def toggle_bot(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    query = update.callback_query
+
+    await query.answer()
+
+
+    async with await connect_db() as db:
+
+        await db.execute(
+            """
+            INSERT INTO settings(key,value)
+            VALUES('status','off')
+            ON CONFLICT(key)
+            DO UPDATE SET value='off'
+            """
+        )
+
+        await db.commit()
+
+
+    await query.edit_message_text(
+        "❌ ربات خاموش شد"
+    )
