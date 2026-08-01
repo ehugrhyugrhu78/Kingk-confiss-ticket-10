e4ddb9ed7293297effffba783c11ea4e6e5b005e
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 00:59:02 +0330

    Update user.py

--- keyboards/user.py
+++ keyboards/user.py
@@ -1,1 +1,28 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+from core.builder import get_buttons
+
+
+
+async def main_keyboard():
+
+    buttons = await get_buttons()
+
+    keyboard = []
+
+
+    for text, action in buttons:
+
+        keyboard.append(
+            [
+                InlineKeyboardButton(
+                    text,
+                    callback_data=action
+                )
+            ]
+        )
+
+
+    return InlineKeyboardMarkup(
+        keyboard
+    )
