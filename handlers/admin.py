8f97e0d0a660d0b906cdd6c61c2d90e8af46c5c2
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 01:02:22 +0330

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,28 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from config import ADMIN_ID
+from keyboards.admin import admin_keyboard
+
+
+
+async def admin_panel(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    user = update.effective_user
+
+
+    if user.id != ADMIN_ID:
+        return
+
+
+    await update.message.reply_text(
+        """
+👑 پنل مدیریت KingK
+
+تمام امکانات ربات از اینجا کنترل می‌شود 🚀
+""",
+        reply_markup=admin_keyboard()
+    )
