fcc9a7ab2f4923454c0a166104592d87be627814
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 01:14:40 +0330

    Update admin_tickets.py

--- keyboards/admin_tickets.py
+++ keyboards/admin_tickets.py
@@ -1,1 +1,37 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+
+def admin_ticket_menu():
+
+    return InlineKeyboardMarkup([
+
+        [
+            InlineKeyboardButton(
+                "📂 همه تیکت‌ها",
+                callback_data="all_tickets"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🟢 تیکت‌های باز",
+                callback_data="open_tickets"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🔴 تیکت‌های بسته",
+                callback_data="closed_tickets"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "⬅️ برگشت",
+                callback_data="admin_back"
+            )
+        ]
+
+    ])
