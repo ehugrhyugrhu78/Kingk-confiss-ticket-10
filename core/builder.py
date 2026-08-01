ae2ab66ca14e5871ee4cb5d50bbd9923b38677d5
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 00:58:14 +0330

    Update builder.py

--- core/builder.py
+++ core/builder.py
@@ -1,1 +1,51 @@
+from database.db import connect_db
 
+
+async def get_buttons():
+
+    async with await connect_db() as db:
+
+        cursor = await db.execute(
+            """
+            SELECT text, action
+            FROM buttons
+            WHERE enabled=1
+            ORDER BY position ASC
+            """
+        )
+
+        return await cursor.fetchall()
+
+
+
+async def add_button(text, action, position=0):
+
+    async with await connect_db() as db:
+
+        await db.execute(
+            """
+            INSERT INTO buttons
+            (text,action,position)
+            VALUES(?,?,?)
+            """,
+            (
+                text,
+                action,
+                position
+            )
+        )
+
+        await db.commit()
+
+
+
+async def delete_button(button_id):
+
+    async with await connect_db() as db:
+
+        await db.execute(
+            "DELETE FROM buttons WHERE id=?",
+            (button_id,)
+        )
+
+        await db.commit()
