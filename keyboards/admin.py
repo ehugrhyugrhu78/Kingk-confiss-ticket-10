7d89d97736a301e6579d5685b2a8c94e8f087080
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 01:55:24 +0330

    Add admin keyboard for Telegram bot

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -1,1 +1,25 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+def admin_keyboard():
+
+    return InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(
+                "🎟 مدیریت تیکت‌ها",
+                callback_data="admin_tickets"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "⚙️ تنظیمات",
+                callback_data="settings"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "🎨 ویرایش ربات",
+                callback_data="editor"
+            )
+        ]
+    ])
