07159fedb66943dc487e9da24fbf0bf0909a39f4
ehugrhyugrhu78 <fyggggg44s8hrrn@atomicmail.io>
2026-08-02 00:55:52 +0330

    Update messages.py

--- core/messages.py
+++ core/messages.py
@@ -1,1 +1,39 @@
+from database.db import connect_db
 
+
+async def get_message(key, default=""):
+
+    async with await connect_db() as db:
+
+        cursor = await db.execute(
+            "SELECT text FROM messages WHERE key=?",
+            (key,)
+        )
+
+        result = await cursor.fetchone()
+
+        if result:
+            return result[0]
+
+    return default
+
+
+
+async def set_message(key, text):
+
+    async with await connect_db() as db:
+
+        await db.execute(
+            """
+            INSERT INTO messages(key,text)
+            VALUES(?,?)
+            ON CONFLICT(key)
+            DO UPDATE SET text=excluded.text
+            """,
+            (
+                key,
+                text
+            )
+        )
+
+        await db.commit()
